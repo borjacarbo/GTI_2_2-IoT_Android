0a15fce359eb453ea905ee4e8f796141177bf19a
EPSG - GTI <epsg@Kubuntu-18.4>
2018-09-26 00:53:06 +0200

    Initial commit

---
+++ app/src/test/java/com/gti/team2/iot/gti_2_2_iot_android/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.gti.team2.iot.gti_2_2_iot_android;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
